6dc7369f166db59f1a58dba6aeefdae1f2a7df90
zainalbani <105286013+zainalbani@users.noreply.github.com>
2022-07-02 09:52:28 +0700

    edit base url

--- lib/constant/Bantuan.dart
+++ lib/constant/Bantuan.dart
@@ -1,9 +1,21 @@
-String baseURL = "http://localhost:8080/final_projectpmo_if20a_zainalbani/public/";
+// String baseURL = "http://localhost:8080/final_projectpmo_if20a_zainalbani/public/";
 // String baseURL = "http://127.0.0.1:8080/final_projectpmo_if20a_zainalbani/public/";
 // String baseURL = "http://192.168.146.126:8080/final_projectpmo_if20a_zainalbani/public/";
+//String baseURL = "http://localhost/rest_api_finalproject/public/";
+String baseURL = "http://10.0.2.2/rest_api_finalproject/public/";
 //Users
 String gid = "";
 String gusername = "";
 String gpassword = "";
 String gemail    = "";
 String grepassword = "";
+String gjeniskucing = "";
+String gjeniskucing1 = "";
+String gjeniskucing2 = "";
+String gjeniskucing3 = "";
+
+
+String gpenjelasan = "";
+String gpenjelasan1 = "";
+String gpenjelasan2 = "";
+String gpenjelasan3 = "";
